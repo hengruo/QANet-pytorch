e9af5976c275fc2cb2e1ff173f40f32075865a09
hengruo <hengruoz@gmail.com>
2018-04-27 16:45:29 -0400

    modified:   README.md 	modified:   models.py

--- README.md
+++ README.md
@@ -1,34 +1,21 @@
-# ZZXproject
+# QANet-pytorch
 
 ## Usage
 
-Python 3.5/3.6 & PyTorch 0.3.1
+Python 3.6 & PyTorch 0.4
 
-1. Install pytorch 0.3.1 for Python 3.5+
-2. Run `pip3 install spacy tqdm ujson requests`
-3. Run `python3 main.py`
+1. Install pytorch 0.4 for Python 3.6+
+2. Run `pip install spacy tqdm ujson requests`
+3. Run `python -m spacy download en`
+4. Run `python main.py`
 
 ## Structure
 dataset.py: download dataset and parse.
 
 main.py: program entry.
 
-models.py: R-net structure.
-
-error_analysis.py: analyze error answers
+models.py: QANet structure.
 
 ## Checkpoints
-**20180226**
-
-Implementation of R-net in PyTorch without self-attention.
-
-[Natural Language Computing Group, MSRA: R-NET: Machine Reading Comprehension with Self-matching Networks](https://www.microsoft.com/en-us/research/publication/mrc/)
-
-
-**20180326**
-
-Complete implementation.
-
-**20180401**
-
-TODO: new embedding for numbers.
+**20180426**
+To implement standard QANet.
--- models.py
+++ models.py
@@ -18,5 +18,46 @@ emb_size = config.emb_size
 
 device = config.device
 
-class QANet(nn.Module):
-    pass
+# Using bidirectional gru hidden state to represent char embedding for a word
+class CharEmbedding(nn.Module):
+    def __init__(self, in_size=word_emb_size):
+        super(CharEmbedding, self).__init__()
+        self.num_layers = 1
+        self.bidirectional = True
+        self.dir = 2 if self.bidirectional else 1
+        self.hidden_size = char_hidden_size
+        self.in_size = in_size
+        self.gru = nn.GRU(input_size=in_size, bidirectional=self.bidirectional, num_layers=self.num_layers, hidden_size=self.hidden_size)
+        self.h = torch.randn(self.num_layers*self.dir, 1, self.hidden_size)
+        self.out_size = self.hidden_size * self.num_layers * self.dir
+
+    def forward(self, input):
+        (l, b, in_size) = input.size()
+        assert in_size == self.in_size and b == 1
+        o, h = self.gru(input, self.h)
+        h = h.view(-1)
+        return h
+
+class NormLayer(nn.Module):
+    def __init__(self, eps=1e-6):
+        super().__init__()
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return (x - mean) / (std + self.eps)
+
+class ConvLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.norm = NormLayer()
+        self.conv = nn.Conv1d()
+
+class EncoderBlock(nn.Module):
+    def __init__(self, conv_num: int):
+        pass
+
+class StandardQANet(nn.Module):
+    def __init__(self):
+        pass
