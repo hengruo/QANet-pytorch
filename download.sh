ecf1775b07d09e21e79f0c4ea551ea56d160e04c
hengruo <hengruoz@gmail.com>
2018-06-14 23:30:08 +0800

    modified:   config.py 	modified:   download.sh

--- download.sh
+++ download.sh
@@ -9,10 +9,10 @@ wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $SQUAD_
 # Download GloVe
 GLOVE_DIR=./data/glove
 mkdir -p $GLOVE_DIR
-# wget http://nlp.stanford.edu/data/glove.840B.300d.zip -O $GLOVE_DIR/glove.840B.300d.zip
-# unzip $GLOVE_DIR/glove.840B.300d.zip -d $GLOVE_DIR
-wget http://nlp.stanford.edu/data/glove.6B.zip -O $GLOVE_DIR/glove.6B.zip
-unzip $GLOVE_DIR/glove.6B.zip -d $GLOVE_DIR
+wget http://nlp.stanford.edu/data/glove.840B.300d.zip -O $GLOVE_DIR/glove.840B.300d.zip
+unzip $GLOVE_DIR/glove.840B.300d.zip -d $GLOVE_DIR
+#wget http://nlp.stanford.edu/data/glove.6B.zip -O $GLOVE_DIR/glove.6B.zip
+#unzip $GLOVE_DIR/glove.6B.zip -d $GLOVE_DIR
 
 # Download Glove Character Embedding
 # wget https://raw.githubusercontent.com/minimaxir/char-embeddings/master/glove.840B.300d-char.txt -O $GLOVE_DIR/glove.840B.300d-char.txt
